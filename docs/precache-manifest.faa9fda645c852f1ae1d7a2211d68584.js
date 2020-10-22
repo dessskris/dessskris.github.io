9a5a814e060d883991c739350517416408176d0a
Desy Kristianti <dessskris@gmail.com>
2020-10-22 21:11:28 +0100

    Built to docs

---
+++ docs/precache-manifest.faa9fda645c852f1ae1d7a2211d68584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebcd16241a7d6c5f739997ae3516b98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c207ec08243d49c694ab",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a214d0595b23e6dca660",
+    "url": "/static/js/2.913612b4.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.913612b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c207ec08243d49c694ab",
+    "url": "/static/js/main.c3c120d6.chunk.js"
+  },
+  {
+    "revision": "f609d5e945159510e3ae",
+    "url": "/static/js/runtime-main.8b00fb54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
